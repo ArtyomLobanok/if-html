187bf636618ec9ecc3af67e1090ec2d9afa53cc7
ArtyomLobanok <hejlenoctb3@gmail.com>
2021-11-01 23:04:56 +0300

    rework section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*Slider loves*/
 $(function() {
 
   $('.overviews__slider').slick({
@@ -36,18 +37,61 @@ $(function() {
     ]
   });
 
-    /*Collapse*/
-    $("[data-collapse]").on("click", function(event) {
-        event.preventDefault();
+    /*Collapse destinations*/
 
-        var $this = $(this),
-            blockId = $this.data('collapse');
-
-        $this.toggleClass("active");
+    $(document).ready(function(){
+        $(".destinations__btn__next").click(function(){
+            $(".destinations__wrapper__second").toggleClass("active"); return false;
+        });
+    });
+    $(document).ready(function(){
+        $(".destinations__btn__next").click(function(){
+            $(".destinations__btn__next").toggleClass("active"); return false;
+        });
+    });
+    $(document).ready(function(){
+        $(".destinations__btn__next").click(function(){
+            $(".destinations__arrow").toggleClass("active"); return false;
+        });
     });
 
+});
 
 
+$(function() {
 
-
+    $('.reviews__block').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow: "<img src='../assets/images/pref.svg' class='overviews__prev' alt='1'>",
+        nextArrow: "<img src='../assets/images/next.svg' class='overviews__next' alt='2'>",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 360,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+        ]
+    });
 });
