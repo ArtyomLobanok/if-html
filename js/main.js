db84e17c9a72666580b1bd38f1efa7652947bc2e
ArtyomLobanok <hejlenoctb3@gmail.com>
2021-11-04 11:54:51 +0300

    fixed bag for arrow on bp360

--- js/main.js
+++ js/main.js
@@ -5,9 +5,7 @@ $(function () {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        autoplay: true,
         speed: 1000,
-        autoPlayTimer: 2000,
         responsive: [
             {
                 breakpoint: 1024,
@@ -60,11 +58,11 @@ $(function () {
 });
 
 /*Carousel Reviews responsive*/
-$('.destinations__block').slick({
+$('.destinations__slider').slick({
     infinite: true,
     draggable: false,
     swipe: false,
-    speed: 300,
+    speed: 1000,
     slidesToShow: 4,
     slidesToScroll: 1,
     arrows: false,
@@ -157,40 +155,6 @@ $('.reviews__block').slick({
                 slidesToScroll: 1,
                 dots: true,
                 arrows: false,
-            }
-        }
-    ]
-});
-
-$('.autoplay').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: false,
-    prevArrow: "<img src='../assets/images/pref.svg' class='reviews__prev' alt='1'>",
-    nextArrow: "<img src='../assets/images/next.svg' class='reviews__next' alt='2'>",
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-            }
-        },
-        {
-            breakpoint: 360,
-            settings: {
-                slidesToShow: 1,
             }
         }
     ]
