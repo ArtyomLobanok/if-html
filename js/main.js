26641e35b5ed702d756dde31eb531319ac1c0bba
Артём Лобанок <91745257+ArtyomLobanok@users.noreply.github.com>
2021-11-07 21:30:32 +0300

    Processing... don't watch please)) (#15)
    * rework section
    
    * compleate reviews slider
    
    * reworked
    
    * reworked
    
    * section destinations is comleate
    
    * section destinations reworked
    
    * fixed bag for arrow on bp360
    
    * fixed bag height 100% for item
    
    * change after element width nav:link and H2
    
    * comleate tab but broke the slider when tabÐ
    
    * fixed tab+slider for section destinations
    
    * fixed input form
    
    * fixed height block rewiews

--- js/main.js
+++ js/main.js
@@ -1,53 +1,247 @@
-$(function() {
+/*Carousel loves responsive*/
+$(function () {
 
-  $('.overviews__slider').slick({
+    $('.overviews__slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 1000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 360,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+        ]
+    });
+
+    /*Tab destinations*/
+    document.querySelectorAll('.tabs-triggers__item').forEach((item) =>
+        item.addEventListener('click', function (e) {
+            e.preventDefault();
+            const id = e.target.getAttribute('href').replace('#', '');
+
+            document.querySelectorAll('.tabs-triggers__item').forEach(
+                (child) => child.classList.remove('tabs-triggers__item--active')
+            );
+
+            document.querySelectorAll('.tabs-content__item').forEach(
+                (child) => child.classList.remove('tabs-content__item--active')
+            );
+
+            item.classList.add('tabs-triggers__item--active');
+            document.getElementById(id).classList.add('tabs-content__item--active');
+        })
+
+    );
+
+//Если нажали на Tabs
+    $('.tabs-triggers__item').on('click', function () {
+        $('.destinations__slider').slick('unslick');
+        $('.destinations__slider').slick({
+            infinite: true,
+            draggable: false,
+            swipe: false,
+            speed: 1000,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: false,
+            variableWidth: false,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        arrows: true,
+                        draggable: true,
+                        swipe: true,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        arrows: true,
+                        draggable: true,
+                        swipe: true,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        arrows: true,
+                        draggable: true,
+                        swipe: true,
+                    }
+                },
+                {
+                    breakpoint: 360,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        arrows: true,
+                        draggable: true,
+                        swipe: true,
+                    }
+                }
+            ]
+        });
+    });
+
+    document.querySelector('.tabs-triggers__item').click();
+
+    /*Collapse destinations*/
+    $(document).ready(function () {
+        $(".destinations__btn__next").click(function () {
+            $(".destinations__wrapper__second").toggleClass("active");
+            return false;
+        });
+    });
+    $(document).ready(function () {
+        $(".destinations__btn__next").click(function () {
+            $(".destinations__btn__next").toggleClass("active");
+            return false;
+        });
+    });
+    $(document).ready(function () {
+        $(".destinations__btn__next").click(function () {
+            $(".destinations__arrow").toggleClass("active");
+            return false;
+        });
+    });
+
+});
+
+/*Carousel Reviews responsive*/
+    $('.destinations__slider').slick({
+        infinite: true,
+        draggable: false,
+        swipe: false,
+        speed: 1000,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        variableWidth: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    draggable: true,
+                    swipe: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    draggable: true,
+                    swipe: true,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    draggable: true,
+                    swipe: true,
+                }
+            },
+            {
+                breakpoint: 360,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    draggable: true,
+                    swipe: true,
+                }
+            }
+        ]
+    });
+
+
+$('.reviews__block').slick({
+    dots: false,
     infinite: true,
-	slidesToShow: 4,
-	slidesToScroll: 1,
-      autoplay: true,
-    autoPlayTimer: 2000,
-      prevArrow: "<img src='../assets/images/pref.svg' class='overviews__prev' alt='1'>",
-      nextArrow: "<img src='../assets/images/next.svg' class='overviews__next' alt='2'>",
-	responsive: [
-	    {
-	      breakpoint: 1024,
-	      settings: {
-	        slidesToShow: 3,
-	      }
-	    },
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: false,
+    rows: 0,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows: false,
+            }
+        },
         {
             breakpoint: 768,
             settings: {
-              slidesToShow: 2,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false
             }
-          },
-	    {
-	      breakpoint: 480,
-	      settings: {
-	        slidesToShow: 2,
-	      }
-	    },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false,
+            }
+        },
         {
             breakpoint: 360,
             settings: {
-            slidesToShow: 2,
-          }
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false,
+            }
         }
     ]
-  });
-
-    /*Collapse*/
-    $("[data-collapse]").on("click", function(event) {
-        event.preventDefault();
-
-        var $this = $(this),
-            blockId = $this.data('collapse');
-
-        $this.toggleClass("active");
+})
+    .on('setPosition', function (event, slick) {
+        slick.$slides.css('height', slick.$slideTrack.height() + 'px');
     });
 
 
 
 
-
-});
