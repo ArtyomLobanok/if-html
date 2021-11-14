6ef77659808d72849bd6a281b4b0573df86cba35
ArtyomLobanok <hejlenoctb3@gmail.com>
2021-11-14 20:42:21 +0300

    rework on Saas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*Carousel loves responsive*/
+/*Carousel overviews responsive*/
 $(function () {
 
     $('.overviews__slider').slick({
@@ -34,100 +34,101 @@ $(function () {
         ]
     });
 
-    /*Tab destinations*/
-    document.querySelectorAll('.tabs-triggers__item').forEach((item) =>
-        item.addEventListener('click', function (e) {
-            e.preventDefault();
-            const id = e.target.getAttribute('href').replace('#', '');
+     /*Tab destinations*/
+     document.querySelectorAll('.tabs-triggers__item').forEach((item) =>
+     item.addEventListener('click', function (e) {
+         e.preventDefault();
+         const id = e.target.getAttribute('href').replace('#', '');
 
-            document.querySelectorAll('.tabs-triggers__item').forEach(
-                (child) => child.classList.remove('tabs-triggers__item--active')
-            );
+         document.querySelectorAll('.tabs-triggers__item').forEach(
+             (child) => child.classList.remove('tabs-triggers__item--active')
+         );
 
-            document.querySelectorAll('.tabs-content__item').forEach(
-                (child) => child.classList.remove('tabs-content__item--active')
-            );
+         document.querySelectorAll('.tabs-content__item').forEach(
+             (child) => child.classList.remove('tabs-content__item--active')
+         );
 
-            item.classList.add('tabs-triggers__item--active');
-            document.getElementById(id).classList.add('tabs-content__item--active');
-        })
+         item.classList.add('tabs-triggers__item--active');
+         document.getElementById(id).classList.add('tabs-content__item--active');
+     })
 
-    );
+ );
 
 //Если нажали на Tabs
-    $('.tabs-triggers__item').on('click', function () {
-        $('.destinations__slider').slick('unslick');
-        $('.destinations__slider').slick({
-            infinite: true,
-            draggable: false,
-            swipe: false,
-            speed: 1000,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            arrows: false,
-            variableWidth: false,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                        arrows: true,
-                        draggable: true,
-                        swipe: true,
-                    }
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        arrows: true,
-                        draggable: true,
-                        swipe: true,
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        arrows: true,
-                        draggable: true,
-                        swipe: true,
-                    }
-                },
-                {
-                    breakpoint: 360,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        arrows: true,
-                        draggable: true,
-                        swipe: true,
-                    }
-                }
-            ]
-        });
-    });
+ $('.tabs-triggers__item').on('click', function () {
+     $('.destinations__slider').slick('unslick');
+     $('.destinations__slider').slick({
+         infinite: true,
+         draggable: false,
+         swipe: false,
+         speed: 1000,
+         slidesToShow: 4,
+         slidesToScroll: 1,
+         arrows: false,
+         variableWidth: false,
+         responsive: [
+             {
+                 breakpoint: 1024,
+                 settings: {
+                     slidesToShow: 3,
+                     slidesToScroll: 1,
+                     arrows: true,
+                     draggable: true,
+                     swipe: true,
+                 }
+             },
+             {
+                 breakpoint: 768,
+                 settings: {
+                     slidesToShow: 2,
+                     slidesToScroll: 1,
+                     arrows: true,
+                     draggable: true,
+                     swipe: true,
+                 }
+             },
+             {
+                 breakpoint: 480,
+                 settings: {
+                     slidesToShow: 2,
+                     slidesToScroll: 1,
+                     arrows: true,
+                     draggable: true,
+                     swipe: true,
+                 }
+             },
+             {
+                 breakpoint: 360,
+                 settings: {
+                     slidesToShow: 2,
+                     slidesToScroll: 1,
+                     arrows: true,
+                     draggable: true,
+                     swipe: true,
+                 }
+             }
+         ]
+     });
+ });
 
-    document.querySelector('.tabs-triggers__item').click();
+ document.querySelector('.tabs-triggers__item').click();
+
 
     /*Collapse destinations*/
     $(document).ready(function () {
-        $(".destinations__btn__next").click(function () {
+        $(".destinations__collapse").click(function () {
             $(".destinations__wrapper__second").toggleClass("active");
             return false;
         });
     });
     $(document).ready(function () {
-        $(".destinations__btn__next").click(function () {
-            $(".destinations__btn__next").toggleClass("active");
+        $(".destinations__collapse").click(function () {
+            $(".destinations__collapse").toggleClass("active");
             return false;
         });
     });
     $(document).ready(function () {
-        $(".destinations__btn__next").click(function () {
+        $(".destinations__collapse").click(function () {
             $(".destinations__arrow").toggleClass("active");
             return false;
         });
